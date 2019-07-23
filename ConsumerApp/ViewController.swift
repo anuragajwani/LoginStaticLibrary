1e3fe83a779246437bbd2c5a60cd8c0367dafd37
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-07-23 23:04:14 +0100

    Added connection to manage sign in button tap in view controller

--- ConsumerApp/ViewController.swift
+++ ConsumerApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onSignInTapped(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
