a5ec36017fe2bf344be59b0b8ad641d88f0d15b4
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-07-23 22:39:08 +0100

    Created Xcode project using Cocoa Touch Static Library template

---
+++ LoginStaticLibrary/LoginStaticLibrary.swift
@@ -0,0 +1,11 @@
+//
+//  LoginStaticLibrary.swift
+//  LoginStaticLibrary
+//
+//  Created by Anurag Ajwani on 23/07/2019.
+//  Copyright © 2019 Anurag Ajwani. All rights reserved.
+//
+
+class LoginStaticLibrary {
+
+}
