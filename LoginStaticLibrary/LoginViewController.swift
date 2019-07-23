a43e716ea26124617b01cb39cadaad73cc1274d4
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-07-23 22:52:15 +0100

    Created LoginViewController

---
+++ LoginStaticLibrary/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  LoginStaticLibrary
+//
+//  Created by Anurag Ajwani on 23/07/2019.
+//  Copyright © 2019 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
