4ed79fd5eade4a67b70a04d5145e60b04e1628df
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-07-23 23:02:14 +0100

    Create consumer app target

---
+++ ConsumerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsumerApp
+//
+//  Created by Anurag Ajwani on 23/07/2019.
+//  Copyright © 2019 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
